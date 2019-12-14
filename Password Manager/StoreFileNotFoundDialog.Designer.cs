b7309cea33348b62ce37d31ee51b11a6712171e4
Christoph <katsurajp@gmail.com>
2019-12-14 12:26:34 +0100

    Rechtschreibfehler

--- Password Manager/StoreFileNotFoundDialog.Designer.cs
+++ Password Manager/StoreFileNotFoundDialog.Designer.cs
@@ -47,7 +47,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(221, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Neue Datei erstellen oder vorhandene Datei?";
+            this.label2.Text = "Neue Datei erstellen oder vorhandene Datei öffnen?";
             // 
             // ButtonNewFile
             // 
